1d8ed5a18b1031f5b4bc9e08290c3ab94812b162
EmilioNacato <ejnacato@espe.edu.ec>
2023-06-14 18:59:52 -0500

    Agrego la carpeta con la solucion del area del Circulo

--- Colaboradores/Emilio-Niacato/Variables/Program.cs
+++ Colaboradores/Emilio-Niacato/Variables/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Crear un script que calcule el area de un rectangulo
+
+double a = 0;
+double b =0;
+double resultado = 0;
+
+console.WriteLine("Por favor ingrese un valor");
+a = Convert.ToInt32(Console.ReadLine());
+
+console.WriteLine("Por favor ingrese un valor");
+b = Convert.ToInt32(Console.ReadLine());
+
+resultado = a * b;
+
+Console.WriteLine("El resultado es: " + resultado);
+
